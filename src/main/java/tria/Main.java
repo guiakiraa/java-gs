f77145c7ad4083ba909b5393a0bf9e3878765c82
guiakiraa <guilhermeakira0810@gmail.com>
2024-11-22 13:10:41 -0300

    fix: cors

--- src/main/java/tria/Main.java
+++ src/main/java/tria/Main.java
@@ -1,8 +1,12 @@
 package tria;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.jsonb.JsonBindingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+import tria.filter.CorsFilter;
 
 import java.io.IOException;
 import java.net.URI;
@@ -12,6 +16,8 @@ import java.net.URI;
  *
  */
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/";
 
@@ -23,6 +29,8 @@ public class Main {
         // create a resource config that scans for JAX-RS resources and providers
         // in tria package
         final ResourceConfig rc = new ResourceConfig().packages("tria");
+        rc.register(JsonBindingFeature.class);
+        rc.register(CorsFilter.class);
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
@@ -35,11 +43,17 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with endpoints available at "
-                + "%s%nHit Ctrl-C to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
+        try {
+            logger.info("SISTEMA INICIANDO...");
+            final HttpServer server = startServer();
+            System.out.println(String.format("Jersey app started with endpoints available at "
+                    + "%s%nHit Ctrl-C to stop it...", BASE_URI));
+            System.in.read();
+            server.stop();
+            logger.info("SISTEMA ENCERRADO");
+        }
+        catch (Exception e) {
+            logger.error("Erro inesperado: " + e);
+        }
     }
 }
-
